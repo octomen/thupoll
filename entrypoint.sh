b97194ec4c332caf6b2af50fc406c2a0b78e0d35
perminovs <perminov-s-i@yandex.ru>
2019-05-23 21:12:18 +0500

    gunicorn logformat (#46)

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,7 +21,11 @@ init_db() {
 
 start_api() {
     # start server
-    exec gunicorn -w 4 -b 0.0.0.0:5000 --access-logfile - --error-logfile - "thupoll.app_factory:init_app()"
+    exec gunicorn -w 4 -b 0.0.0.0:5000 \
+      --access-logfile - \
+      --error-logfile - \
+      --access-logformat "%(h)s %(l)s %(u)s %(t)s pid %(p)s \"%(r)s\" %(s)s %(b)s \"%(f)s\" \"%(a)s\" %(l)s %(D)s µs" \
+      "thupoll.app_factory:init_app()"
 }
 
 # Start specified service
