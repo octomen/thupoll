af353953256aa1a9391c98a7245fe7022688cf4d
Daniil Galiev <dgaliev@rc-online.org>
2019-06-13 14:00:47 +0500

    some elegance

--- tests/units/test_di.py
+++ tests/units/test_di.py
@@ -4,7 +4,7 @@ from thupoll.utils import di
 
 
 def provider(a, b):
-    return a+b
+    return a + b
 
 
 def container_factory(factory_class, x, y):
@@ -21,14 +21,14 @@ params = pytest.mark.parametrize("factory_class", [di.Factory, di.Singleton])
 def test__simple_run(factory_class, faker):
     x, y = faker.pyint(), faker.pyint()
     factory = factory_class(provider, a=x, b=y)
-    assert factory() == x+y
+    assert factory() == x + y
 
 
 @params
 def test__override_kw(factory_class, faker):
     x, y = faker.pyint(), faker.pyint()
     factory = factory_class(provider, a=y)
-    assert factory(a=x, b=y) == x+y
+    assert factory(a=x, b=y) == x + y
 
 
 @params
@@ -37,9 +37,9 @@ def test__override_factory(factory_class, faker):
     factory = factory_class(provider, a=x, b=x)
     factory.override(factory_class(provider, a=y, b=y))
     factory.override(factory_class(provider, a=z, b=z))
-    assert factory(b=x) == z+x
+    assert factory(b=x) == z + x
     factory.reset()
-    assert factory() == y+y
+    assert factory() == y + y
 
 
 def test__singleton(faker):
@@ -53,7 +53,7 @@ def test__dependencies(factory_class, faker):
     x, y = faker.pyint(), faker.pyint()
     child_factory = factory_class(provider, a=x, b=x)
     parent_factory = factory_class(provider, a=y, b=child_factory)
-    assert parent_factory() == x+x+y
+    assert parent_factory() == x + x + y
 
 
 @params
@@ -61,21 +61,21 @@ def test__container(factory_class, faker):
     x, y = faker.pyint(), faker.pyint()
     container = container_factory(factory_class, x, y)
 
-    assert container.parent_factory() == x+x+y
+    assert container.parent_factory() == x + x + y
 
 
 def test__container_override(faker):
     x, y = faker.pyint(), faker.pyint()
 
     container = container_factory(di.Factory, x, y)
-    assert container.parent_factory() == x+x+y
+    assert container.parent_factory() == x + x + y
 
     @container.override
     class _:
         child_factory = di.Factory(provider, a=y, b=y)
 
-    assert container.parent_factory() == y+y+y
+    assert container.parent_factory() == y + y + y
 
     container.reset()
 
-    assert container.parent_factory() == x+x+y
+    assert container.parent_factory() == x + x + y
